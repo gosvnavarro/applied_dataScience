adc07afa54d4e50e256a4d6f890682a6efbd1840
_gosvn <gabi.onavarro@gmail.com>
2022-02-04 10:51:24 -0300

    Update week_1.py

--- week_1.py
+++ week_1.py
@@ -1,10 +1,10 @@
-# Function one
+# Funcao 1 -
 def add_numbers(x, y, z):
-    return x+y+z
+    return x + y + z
 
 print(add_numbers(1, 2, 3))
 
-# Function two
+# Funcao 2 - Conditional
 def do_math(a, b, kind):
     if (kind == 'add'):
         return a + b
@@ -12,3 +12,22 @@ def do_math(a, b, kind):
         return a - b
 
 do_math(1, 2, kind = 'add')
+
+# Funcao 3 - Separacao de nomes
+people = ['Dr. Christopher Brooks', 'Dr. Kevyn Collins-Thompson', 'Dr. VG Vinod Vydiswaran', 'Dr. Daniel Romero']
+
+def split_title_and_name(person):
+    return person.split()
+
+list(map(split_title_and_name, people))
+
+# OU
+
+people = ['Dr. Christopher Brooks', 'Dr. Kevyn Collins-Thompson', 'Dr. VG Vinod Vydiswaran', 'Dr. Daniel Romero']
+
+def split_title_and_name(person):
+    title = person.split()[0]
+    lastname = person.split()[-1]
+    return '{} {}'.format(title, lastname)
+
+list(map(split_title_and_name, people))
