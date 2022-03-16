98187f00db452fba8f15a1b313b0a3c7b8a60103
_gosvn <gabi.onavarro@gmail.com>
2022-03-16 08:04:09 -0300

    Update assignment_4.py

--- assignment_4.py
+++ assignment_4.py
@@ -151,3 +151,60 @@ def mlb_correlation():
     return stats.pearsonr(population_by_region, win_loss_by_region)[0]
 
 mlb_correlation()
+
+# Question 4
+import pandas as pd
+import numpy as np
+import scipy.stats as stats
+import re
+
+cities = pd.read_html("assets/wikipedia_data.html")[1]
+cities = cities.iloc[:-1, [0, 3, 5, 6, 7, 8]]
+cities.rename(columns = {"Population (2016 est.)[8]": "Population"}, inplace = True)
+cities['NFL'] = cities['NFL'].str.replace(r"\[.*\]", "")
+cities['MLB'] = cities['MLB'].str.replace(r"\[.*\]", "")
+cities['NBA'] = cities['NBA'].str.replace(r"\[.*\]", "")
+cities['NHL'] = cities['NHL'].str.replace(r"\[.*\]", "")
+
+Big4 = 'NFL'
+
+def nfl_correlation(): 
+    team = cities[Big4].str.extract('([A-Z]{0,2}[a-z0-9]*\ [A-Z]{0,2}[a-z0-9]*|[A-Z]{0,2}[a-z0-9]*)([A-Z]{0,2}[a-z0-9]*\ [A-Z]{0,2}[a-z0-9]*|[A-Z]{0,2}[a-z0-9]*)([A-Z]{0,2}[a-z0-9]*\ [A-Z]{0,2}[a-z0-9]*|[A-Z]{0,2}[a-z0-9]*)')
+    team['Metropolitan area']=cities['Metropolitan area']
+    team = pd.melt(team, id_vars=['Metropolitan area']).drop(columns = ['variable']).replace("", np.nan).replace("—", np.nan).dropna().reset_index().rename(columns = {"value":"team"})
+    team = pd.merge(team, cities, how = 'left', on = 'Metropolitan area').iloc[:, 1:4]
+    team = team.astype({'Metropolitan area': str, 'team': str, 'Population': int})
+    team['team'] = team['team'].str.replace('[\w.]*\ ', '')
+    
+    _df = pd.read_csv("assets/"+str.lower(Big4)+".csv")
+    _df = _df[_df['year'] == 2018]
+    _df['team'] = _df['team'].str.replace(r'[\*]', "")
+    _df['team'] = _df['team'].str.replace(r'\(\d*\)', "")
+    _df['team'] = _df['team'].str.replace(r'[\xa0]', "")
+    _df = _df[['team','W-L%']]
+    _df.rename(columns={"W-L%": "W/L%"},inplace=True)
+    dropList = []
+    for i in range(_df.shape[0]):
+        row = _df.iloc[i]
+        if row['team'] == row['W/L%']:
+            dropList.append(i)
+    _df = _df.drop(dropList)
+
+    _df['team'] = _df['team'].str.replace('[\w.]* ', '')
+    _df['team'] = _df['team'].str.replace('+', '')
+    _df = _df.astype({'team': str,'W/L%': float})
+    
+    merge = pd.merge(team,_df,'outer', on = 'team')
+    merge = merge.groupby('Metropolitan area').agg({'W/L%': np.nanmean, 'Population': np.nanmean})
+    
+    #raise NotImplementedError()
+    
+    population_by_region = merge['Population'] # pass in metropolitan area population from cities
+    win_loss_by_region = merge['W/L%'] # pass in win/loss ratio from _df in the same order as cities["Metropolitan area"]   
+    
+    assert len(population_by_region) == len(win_loss_by_region), "Q4: Your lists must be the same length"
+    assert len(population_by_region) == 29, "Q4: There should be 29 teams being analysed for NFL"
+
+    return stats.pearsonr(population_by_region, win_loss_by_region)[0]
+
+nfl_correlation()
