1904621775f121387fdee7b94273a19656bbe7ac
_gosvn <gabi.onavarro@gmail.com>
2022-03-25 08:56:35 -0300

    Create assignment_2-1.py

---
+++ assignment_2-1.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import mplleaflet
+import pandas as pd
+
+def leaflet_plot_stations(binsize, hashid):
+
+    df = pd.read_csv('data/C2A2_data/BinSize_d{}.csv'.format(binsize))
+
+    station_locations_by_hash = df[df['hash'] == hashid]
+
+    lons = station_locations_by_hash['LONGITUDE'].tolist()
+    lats = station_locations_by_hash['LATITUDE'].tolist()
+
+    plt.figure(figsize=(8,8))
+
+    plt.scatter(lons, lats, c='r', alpha=0.7, s=200)
+
+    return mplleaflet.display()
+
+leaflet_plot_stations(400,'fb441e62df2d58994928907a91895ec62c2c42e6cd075c2700843b89')
