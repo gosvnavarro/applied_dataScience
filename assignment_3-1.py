d757288480ef8082c417a4fcff8ac85d2f62813b
_gosvn <gabi.onavarro@gmail.com>
2022-04-23 15:20:24 -0300

    Update assignment_3-1.py

--- assignment_3-1.py
+++ assignment_3-1.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
+import math
+from matplotlib import cm
+import matplotlib.pyplot as plt
 
 # Criar a tabela
 np.random.seed(12345)
 df = pd.DataFrame([np.random.normal(32000,200000,3650), np.random.normal(43000,100000,3650), 
                    np.random.normal(43500,140000,3650), np.random.normal(48000,70000,3650)], 
-                  index=[A,B,C,D])
+                  index=['A','B','C','D'])
 df
 
 # Inverter a tabela e ver as metricas
@@ -13,8 +16,6 @@ df = df.transpose()
 df.describe()
 
 # Calcular o intervalo de confianca para cada variavel 
-import math
-
 mean = list(df.mean())
 std = list(df.std())
 letter_1 = []
@@ -25,7 +26,7 @@ for i in range (4) :
 letter_1
 
 nearest = 100
-Y = 39500
+Y = 35000
 
 df_p = pd.DataFrame()
 df_p['diff'] = nearest * ((Y - df.mean())//nearest)
@@ -37,8 +38,6 @@ df_p['shade'] = df_p['sign']*np.interp(df_p['diff'].abs(), old_range, new_range)
 # Cores das barras
 shade = list(df_p['shade'])
 
-from matplotlib import cm
-
 blues = cm.Blues
 reds = cm.Reds
 
@@ -47,8 +46,6 @@ color = ['White' if  x == 0 else reds(abs(x))
          if x<0 else blues(abs(x)) for x in shade]
 
 # Grafico
-import matplotlib.pyplot as plt
-
 %matplotlib inline
 
 plt.figure(num = None, figsize = (6, 6), dpi = 80, facecolor = 'w', edgecolor = 'k')
@@ -56,9 +53,9 @@ plt.bar(range(len(df.columns)), height = df.values.mean(axis = 0),
         yerr = letter_1, error_kw = {'capsize': 10, 'elinewidth': 2, 'alpha':0.7}, color = color)
 
 plt.axhline(y=Y, color = 'black', label = 'Y')
-plt.text(3.5, 40000, "39500")
+plt.text(3.5, 35000, "35000")
 plt.xticks(range(len(df.columns)), df.columns)
-plt.title('Generated Data Between 1992 - 95')
+plt.title('Generated Data between A, B, C and D')
 
 plt.tick_params(top='off', bottom='off',  right='off', labelbottom='on')
 
